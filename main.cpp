c677f2edf618e124683eab916c67ea946bcfbd48
hanjing <hanjing2020@foxmail.com>
2023-11-13 21:41:07 +0800

    draft

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <thread>
 #include <map>
+#include <vector>
+#include <mutex>
+#include <chrono>
+#include <string>
 
 
 struct User {
@@ -14,50 +18,80 @@ struct User {
     std::string phone;
 };
 
-std::map<std::string, User> users;
-std::map<std::string, long> has_login;  // 换成 std::chrono::seconds 之类的
+class HTTPServer {
+private:
+    std::map<std::string, User> users;
+    std::map<std::string, std::chrono::time_point<std::chrono::steady_clock> > has_login;  // 换成 std::chrono::seconds 之类的
+    std::mutex mtx;
 
+public:
 // 作业要求1：把这些函数变成多线程安全的
 // 提示：能正确利用 shared_mutex 加分，用 lock_guard 系列加分
-std::string do_register(std::string username, std::string password, std::string school, std::string phone) {
-    User user = {password, school, phone};
-    if (users.emplace(username, user).second)
-        return "注册成功";
-    else
-        return "用户名已被注册";
-}
-
-std::string do_login(std::string username, std::string password) {
-    // 作业要求2：把这个登录计时器改成基于 chrono 的
-    long now = time(NULL);   // C 语言当前时间
-    if (has_login.find(username) != has_login.end()) {
-        int sec = now - has_login.at(username);  // C 语言算时间差
-        return std::to_string(sec) + "秒内登录过";
+    std::string do_register(std::string username, std::string password, std::string school, std::string phone) {
+        std::unique_lock lock(mtx);
+        User user = {password, school, phone};
+        if (users.emplace(username, user).second)
+            return "注册成功";
+        else
+            return "用户名已被注册";
     }
-    has_login[username] = now;
 
-    if (users.find(username) == users.end())
-        return "用户名错误";
-    if (users.at(username).password != password)
-        return "密码错误";
-    return "登录成功";
-}
+    std::string do_login(std::string username, std::string password) {
+        // 作业要求2：把这个登录计时器改成基于 chrono 的
+        std::unique_lock lock(mtx);
+        auto now = std::chrono::steady_clock::now();
+        if (has_login.find(username) != has_login.end()) {
+            auto sec = now - has_login.at(username);  // C 语言算时间差
+            return std::to_string(std::chrono::duration_cast<std::chrono::seconds>(sec).count()) + "秒内登录过";
+        }
+        has_login[username] = now;
 
-std::string do_queryuser(std::string username) {
-    auto &user = users.at(username);
-    std::stringstream ss;
-    ss << "用户名: " << username << std::endl;
-    ss << "学校:" << user.school << std::endl;
-    ss << "电话: " << user.phone << std::endl;
-    return ss.str();
-}
+        if (users.find(username) == users.end())
+            return "用户名错误";
+        if (users.at(username).password != password)
+            return "密码错误";
+        return "登录成功";
+    }
+
+    std::string do_queryuser(std::string username) {
+        std::unique_lock  lock(mtx);
+        if (users.find(username) == users.end()) {
+            return "unknown";
+        }
+        auto &user = users.at(username);
+        std::stringstream ss;
+        ss << "用户名: " << username << std::endl;
+        ss << "学校:" << user.school << std::endl;
+        ss << "电话: " << user.phone << std::endl;
+        return ss.str();
+    }
+};
+
+HTTPServer http_server;
 
 
 struct ThreadPool {
+private:
+    std::vector<std::thread> m_pool;
+    std::mutex mtx;
+public:
     void create(std::function<void()> start) {
         // 作业要求3：如何让这个线程保持在后台执行不要退出？
         // 提示：改成 async 和 future 且用法正确也可以加分
-        std::thread thr(start);
+        std::unique_lock<std::mutex> lock;
+        if (m_pool.size() >= 4) {
+            for (auto& thr: m_pool) {
+                thr.join();
+            }
+            m_pool.clear();
+        }
+        m_pool.push_back(std::move(std::thread(start)));
+    }
+
+    ~ThreadPool() {
+        for (auto& thr : m_pool) {
+            thr.join();
+        }
     }
 };
 
@@ -74,16 +108,16 @@ std::string phone[] = {"110", "119", "120", "12315"};
 int main() {
     for (int i = 0; i < 262144; i++) {
         tpool.create([&] {
-            std::cout << do_register(test::username[rand() % 4], test::password[rand() % 4], test::school[rand() % 4], test::phone[rand() % 4]) << std::endl;
+            std::cout << http_server.do_register(test::username[rand() % 4], test::password[rand() % 4], test::school[rand() % 4], test::phone[rand() % 4]) << std::endl;
         });
         tpool.create([&] {
-            std::cout << do_login(test::username[rand() % 4], test::password[rand() % 4]) << std::endl;
+            std::cout << http_server.do_login(test::username[rand() % 4], test::password[rand() % 4]) << std::endl;
         });
         tpool.create([&] {
-            std::cout << do_queryuser(test::username[rand() % 4]) << std::endl;
+            std::cout << http_server.do_queryuser(test::username[rand() % 4]) << std::endl;
         });
     }
 
-    // 作业要求4：等待 tpool 中所有线程都结束后再退出
+    // 作业要求4：等待 tpool 中所有线程都结束后再退出 # done
     return 0;
 }
